524c477ab15a21a6f99420369404fb60b1330f1e
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-06-22 15:24:57 -0400

    dark mode toggle switch is functioning!

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-console.log("hey Lizzy");
-
 const darkMode = document.getElementById("change-mode");
 
 darkMode.addEventListener("click", () => {
   console.log("CLICKED");
 
   if (darkMode.checked) {
-    console.log("This is dark mode");
+    toggleDarkMode("dist/css/dark-mode.css", 2);
   } else {
-    console.log("This is light mode");
+    toggleDarkMode("dist/css/light-mode.css", 2);
   }
 });
 
-// function check() {
-//   darkMode.checked = true;
-// }
+function toggleDarkMode(cssFile, cssLinkIndex) {
+  var oldLink = document.getElementsByTagName("link").item(cssLinkIndex);
 
-// function uncheck() {
-//   document.getElementById("myCheck").checked = false;
-// }
+  var newLink = document.createElement("link");
+  newLink.setAttribute("rel", "stylesheet");
+  newLink.setAttribute("type", "text/css");
+  newLink.setAttribute("href", cssFile);
+
+  document.getElementsByTagName("head").item(0).replaceChild(newLink, oldLink);
+}
