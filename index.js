70c8f5a117e35ac0308c8e80330b115c9077ac03
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-06-22 13:56:07 -0400

    colors are set up in both light and dark mode.  Basic javascript started to toggle between light mode css and dark mode css

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("hey Lizzy");
+
+const darkMode = document.getElementById("change-mode");
+
+darkMode.addEventListener("click", () => {
+  console.log("CLICKED");
+
+  if (darkMode.checked) {
+    console.log("This is dark mode");
+  } else {
+    console.log("This is light mode");
+  }
+});
+
+// function check() {
+//   darkMode.checked = true;
+// }
+
+// function uncheck() {
+//   document.getElementById("myCheck").checked = false;
+// }
